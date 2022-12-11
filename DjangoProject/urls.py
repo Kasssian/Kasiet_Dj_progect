1160a3f58853ff72909188fdea6998e807ccddb5
Kasiet <bloodyangel10k@gmail.com>
2022-12-11 14:37:39 +0600

    posts

--- DjangoProject/urls.py
+++ DjangoProject/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from posts.views import hello, now_date, good_bay
 
 urlpatterns = [
+    path('hello/', hello),
     path('admin/', admin.site.urls),
+    path('good_bay/', good_bay),
+    path('now_date/', now_date)
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from datetime import datetime
 
-# Create your views here.
+from django.shortcuts import HttpResponse
+
+
+def hello(requests):
+    return HttpResponse('Hello! Its my project')
+
+
+def good_bay(requests):
+    return HttpResponse('Good bay user!')
+
+
+def now_date(requests):
+    return HttpResponse(datetime.now().date())
